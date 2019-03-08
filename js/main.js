72ac5c5cd62de09366f361505e26a825cbe5e5c0
hamza kouki <hamzakouki33@gmail.com>
2019-03-08 18:16:53 +0100

    new_commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function changeP() {
+        var content = prompt("Please enter the new content of the paragraph");
+        if (content != null) {
+          document.getElementById("changeContent").innerHTML =content;
+         
+        }
+      }
+  //------------------------------------------------
