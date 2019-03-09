0b5676f057aa8f376b9d5f70b0d38a4b8c5be9b4
hamza kouki <hamzakouki33@gmail.com>
2019-03-09 21:58:41 +0100

    last_update

--- js/main.js
+++ js/main.js
@@ -6,7 +6,16 @@ function changeP() {
         }
       }
   //------------------------------------------------
-  var values;
+  var values,new_Class_Name;
+  //------------------------------------------------
+  function concate(values){
+    var str1 = values;
+   var str2 = "_1";
+    //var str3=".";
+  //  var res = str1.concat(str2);
+    new_Class_Name = str1.concat(str2);
+}
+///----------------------------------------------
   function hide_show(values) {
     var x = document.querySelector(values);
     if (x.style.display === "none") {
@@ -15,4 +24,20 @@ function changeP() {
     else{
       x.style.display = "none";
     }
+    ///--------------------------
+    var oldHTML = document.querySelector(values).innerHTML;
+    var newHTML = "" + oldHTML + "";
+    var newHTML = newHTML.replace(/</g,"&lt;");
+    var newHTML = newHTML.replace(/>/g,"&gt;");
+    concate(values);
+    document.querySelector(new_Class_Name).innerHTML = newHTML;
+
 }
+/*
+function hide_show(values){
+    var oldHTML = document.querySelector(values).innerHTML;
+    var newHTML = "" + oldHTML + "";
+    var newHTML = newHTML.replace(/</g,"&lt;");
+    var newHTML = newHTML.replace(/>/g,"&gt;");
+    document.querySelector(values).innerHTML = newHTML;
+}*/
