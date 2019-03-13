732a01db0e2c80298c4d1baa2ede10f3b6da9387
hamza kouki <hamzakouki33@gmail.com>
2019-03-13 21:21:42 +0100

    the-last_commit

--- js/main.js
+++ js/main.js
@@ -1,41 +1,57 @@
-var new_Class_Name , values , content_new;
-function concate(values){
+var values, new_Class_Name;
+//--------------------------------------------
+function concate(values) {
   var str1 = values;
- var str2 = "_1";
+  var str2 = "_1";
   new_Class_Name = str1.concat(str2);
 }
-function display_source(values,new_Class_Name){
-var oldHTML = document.querySelector(values).innerHTML;
-var newHTML = "" + oldHTML + "";
-var newHTML = newHTML.replace(/</g,"&lt;");
-var newHTML = newHTML.replace(/>/g,"&gt;");
-concate(values);
-document.querySelector(new_Class_Name).innerHTML = newHTML;
+//-------------------------------------------------
+
+function display_source(values, new_Class_Name) {
+  var oldHTML = document.querySelector(values).innerHTML;
+  var newHTML = String("" + oldHTML + "")
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;");
+  concate(values);
+  document.querySelector(new_Class_Name).innerHTML = newHTML;
 }
-function view_source(values,new_Class_Name){
-  var content_new = document.querySelector(values).innerHTML;
-  var text = document.createTextNode('<p>'+content_new+'</p>');
-        document.querySelector(new_Class_Name).appendChild(text);
-}
+///----------------------------------------------
 //https://stackoverflow.com/questions/7874598/regular-expression-regular-or-irregular
- view_source('.PParagraphe','.PParagraphe_1');
-function changeP(values, new_Class_Name) {
-      var content = prompt("Please enter the new content of the paragraph");
-      if (content != null) {
-        document.querySelector(values).innerHTML = content;
-    //---------------
-    view_source(values,new_Class_Name);
-      }
-    }
+//---------------------
+function changeP(oldClassName, newClassName) {
+  var content = prompt("Please enter the new content of the paragraph");
+  if (content != null) {
+    // replace <p></p> with spécial caracters to be presented in the <pre></pre> in index.html
+    var newHTML = String("<p>" + content + "</p>")
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;");
+    // replace pparagraphe1 with the new content
+    document.querySelector(oldClassName).innerHTML = newHTML;
+
+    // replace the preview part with the content
+    document.querySelector(newClassName).innerHTML = content;
+  }
+}
+//----------------------------------------------------
 function hide_show(values) {
   var x = document.querySelector(values);
   if (x.style.display === "none") {
     x.style.display = "block";
-  } 
-  else{
+  } else {
     x.style.display = "none";
   }
+  ///--------------------------
+  var oldHTML = document.querySelector(values).innerHTML;
+  var newHTML = "" + oldHTML + "";
+  var newHTML = newHTML.replace(/</g, "&lt;");
+  var newHTML = newHTML.replace(/>/g, "&gt;");
   concate(values);
-  display_source(values,new_Class_Name);
-
+  document.querySelector(new_Class_Name).innerHTML = newHTML;
+  concate(values);
+  display_source(values, new_Class_Name);
 }
+display_source(".PParagraphe", ".PParagraphe_1");
